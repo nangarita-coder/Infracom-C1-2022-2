9ed1e67a3c4602766dc833963851029b2a65cc92
nangarita-coder <60239309+nangarita-coder@users.noreply.github.com>
2022-09-05 10:15:03 -0500

    Metodos basicos
    metodos envio mensajes

--- Caso1/src/Buzon.java
+++ Caso1/src/Buzon.java
@@ -1,4 +1,55 @@
+import java.util.List;
+import java.util.LinkedList;
 
 public class Buzon {
+
+	private List<String> buzon;
+	private int size;
+
+	public Buzon(int size ) {
+		// TODO Auto-generated constructor stub
+		this.size = size;
+		this.buzon = new LinkedList<String>();
+
+
+	}
+
+	public synchronized boolean lleno()
+	{
+		return this.buzon.size()>0;
+	}
+
+	public synchronized void recibirMensaje(String mensaje)
+	{
+		while (this.buzon.size()==this.size) {
+			try {
+				wait();
+
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+
+			this.buzon.add(mensaje);
+			notify();
+		}
+	}
+
+	public synchronized String enviarMensaje()
+	{
+		while (this.buzon.size()==0) {
+			try {
+				wait();
+
+			} catch (Exception e) {
+				// TODO: handle exception
+				e.printStackTrace();
+			}
+			String mensaje = this.buzon.remove(0);
+			notify();
+			return mensaje;
+		}
+		
+	}
 
 }
--- Caso1/src/Proceso.java
+++ Caso1/src/Proceso.java
@@ -1,4 +1,24 @@
 
-public class Proceso {
+public class Proceso extends Thread {
+	private int id;
+	private int N;
+	private int M;
+	private Buzon buz;
+	private String mensaje;
 
+	public Proceso( int id , Buzon buz) {
+		// TODO Auto-generated constructor stub
+		this.id = id;
+		this.buz = buz;
+		
+	}
+	private void enviar(String mensaje)
+	{
+		this.buz.recibirMensaje(mensaje + "T"+ N + M );
+	}
+	@Override
+	public void run() {
+		// TODO Auto-generated method stub
+		super.run();
+	}
 }
